434ec868269aa1746a73a1fd7e9460e16f35d521
ChristianGil <cmg580@utexas.edu>
2019-04-01 09:09:48 -0500

    constructIntenityGraph is finished

--- Project 2/Program2.java
+++ Project 2/Program2.java
@@ -6,29 +6,29 @@ public class Program2 {
 
     public int constructIntensityGraph(int[][] image){
         // Create the edge graph
-        int[][] leftEdges = new int[image.length][image.length];
-        int[][] bottomEdges = new int[image.length][image.length];
+        int[][] leftEdges = new int[image.length][image[0].length];
+        int[][] bottomEdges = new int[image.length][image[0].length];
         
         // For each row of the image
-        for ( i = 0; i < image.length; i++ ){
+        for ( int i = 0; i < image.length; i++ ){
             // For each column in the image
-            for ( j = 0; j < image[i].length; j++ ){
+            for ( int j = 0; j < image[i].length; j++ ){
                 // Populate left edges
                 if (j-1 >= 0){
-                    leftEdges[i][j] = abs( image[i][j-1] - image[i][j] );
+                    leftEdges[i][j] = Math.abs( image[i][j-1] - image[i][j] );
                 }
                 // Populate the bottom edges
-                if (i+1 <= image.length){
-                    bottomEdges[i][j] = abs( image[i+1][j] - image[i][j] );
+                if (i+1 < image.length){
+                    bottomEdges[i][j] = Math.abs( image[i+1][j] - image[i][j] );
                 }
             }
         }
 
         int sum = 0;
         // Sum all the edges
-        for ( i = 0; i < image.length; i++ ){
+        for ( int i = 0; i < image.length; i++ ){
             // For each column in the image
-            for ( j = 0; j < image[i].length; j++ ){
+            for ( int j = 0; j < image[i].length; j++ ){
                 sum += leftEdges[i][j] + bottomEdges[i][j];
             }
         }
