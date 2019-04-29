ac20fed5192289f007f188a6e0b0f155afc42eac
ChristianGil <cmg580@utexas.edu>
2019-04-29 11:20:07 -0500

    Kinda working Part 1

--- Project 3/Program3.java
+++ Project 3/Program3.java
@@ -1,6 +1,8 @@
 import sun.jvm.hotspot.gc.shared.Space;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
 
 //Name: Christian Gil
 //EID: cmg4463
@@ -53,26 +55,34 @@ public class Program3 {
 
         // 2. S <- Empty
         ArrayList<Integer> shortest_path_nodes = new ArrayList<Integer>();
+        ArrayList<Integer> shortest_path_values = new ArrayList<Integer>();
 
         // 3. Q <- G.V
         MinHeap Q = G;
 
         // 4. while Q != Empty
         while (Q.size() != 0){
-            // 5.   do u <- Extract-Min(Q)
+            // 5.   do u <- Extract-Min(Q) // If the min edge is reached, we are done
             Planet u = Q.getMin();
             // 6.     S <- S U {u}
             shortest_path_nodes.add(u.planet);
+            shortest_path_values.add(u.distance);
+            if (u.planet == end){
+                break;
+            }
             // 7.     for each vertex v within Adj[u]
             for (int i = 0; i < connections[u.planet].length; i++){
+                // w is the weight from u to v
+                int w = connections[u.planet][i].getTime();
+                // get the destination planet reachable from the current planet
+                int destination = connections[u.planet][i].getDestination();
                 // 8.       do relax(u, v, w)
-                // relax(u, )
+                relax(Q, u, destination, w);
             }
         }
 
-
-
-        return 0;
+        // Get the length of the path
+        return shortest_path_values.get( shortest_path_values.size() - 1 );
      }
 
     // Create Initialization step
@@ -108,16 +118,21 @@ public class Program3 {
     }
 
 
-    // // Create Relaxation step
-    // public void relax(Planet u, Planet v){
-    //     // if v.d > u.d + w(u,v)
-
-    //     //   then v.d <- u.d + w(u,v)
-
-    //     //     v.pi <- u
-
-
-    // }
+    // Create Relaxation step
+    public void relax(MinHeap minheap, Planet u, int destination, int w){
+        Planet destinationPlanet = minheap.getPlanet(destination); // THIS STEP WILL FAIL IF NO CONNECTION FROM 0 TO THIS PLANET
+        // if v.d > u.d + w(u,v)
+        if (destinationPlanet.distance > (u.distance + w)){
+            //   then v.d <- u.d + w(u,v)
+            destinationPlanet.distance = (u.distance + w);
+            //     v.pi <- u
+            destinationPlanet.pathParent = u;
+        }
+        // Reorganize the heap
+        if (minheap.size() != 0) {
+            minheap.HeapifyUp( minheap.getIndexOfPlanet( destinationPlanet ) );
+        }
+    }
     
     // // Create Extract-Min(Q)
     // public void extract_min(Q){
@@ -155,6 +170,7 @@ class Planet{
     public int planet;
     public int parent;
     public int distance;
+    public Planet pathParent;
 
     Planet(){
         this.planet = -1;
@@ -166,6 +182,11 @@ class Planet{
         this.planet = planet;
         this.parent = parent;
         this.distance = distance;
+    }
+
+    // Overriding the compare method to sort the age 
+    public int compare(Planet P, Planet P1) {
+        return P.planet - P1.planet;
     }
 }
 
@@ -183,7 +204,7 @@ class MinHeap{
     public void createNode(Planet P){
         if (this.node.size() != 0){
             this.node.add(P);
-            HeapifyUp(this.node.size());
+            HeapifyUp(this.node.size() - 1);
         }
         else{
             this.node.add(P);
@@ -192,8 +213,10 @@ class MinHeap{
 
     // Remove a node from the heap
     public Planet getMin(){
-        Planet P = this.node.get(0);
-        HeapifyDown(0);
+        Planet P = this.node.remove(0);
+        if (this.node.size() > 1){
+            HeapifyDown(0);
+        }
         return P;
     }
 
@@ -201,7 +224,6 @@ class MinHeap{
     public void HeapifyUp(int i){
         // If i > 1 then
         if (i > 0){
-            i -= 1;
             // let j = parent(i) = [i/2]
             this.node.get(i).parent = i / 2;
             int j = i / 2;
@@ -221,10 +243,12 @@ class MinHeap{
     public void HeapifyDown(int i){
         // Let n = length(H)
         int n = this.node.size();
+        // persist j
+        int j = -1;
         // If 2i > n then
         if (2*i > n){
             // Terminate with H unchanged
-            break;
+            return;
         }
         // Else if 2i < n then
         else if (2*i < n){
@@ -232,21 +256,21 @@ class MinHeap{
             int left = 2*i;
             int right = 2*i + 1;
             // Let j be the index that minimizes key[H[left]] and key[H[right]]
-            int leftValue = this.node[left].distance;
-            int rightValue = this.node[right].distance;
+            int leftValue = this.node.get(left).distance;
+            int rightValue = this.node.get(right).distance;
             if (leftValue < rightValue){
-                int j = left;
+                j = left;
             }
             else if (rightValue < leftValue){
-                int j = right;
+                j = right;
             }
             else{
-                int j = left;
+                j = right;
             }
         }
         // Else if 2i = n then
         else if (2*i == n){
-            int j = 2*i;
+            j = 2*i;
         }
         // If key[H[j]] < key[H[i]] then
         if (this.node.get(j).distance < this.node.get(i).distance){
@@ -263,5 +287,28 @@ class MinHeap{
     // getter for the size of the array
     public int size(){
         return this.node.size();
+    }
+
+    // Search the list and return the planet
+    public Planet getPlanet(int i){
+        // // Clone the Array
+        // List<Planet> nodes = (ArrayList<Planet>)this.node.clone();
+        // // Sort the nodes
+        // Arrays.sort(nodes, new Planet());
+        // // Use binary search to find the planet
+        // Arrays.binarySearch(nodes, i, new Planet());
+        
+        for (int j = 0; j < this.node.size(); j++){
+            if (this.node.get(j).planet == i){
+                return this.node.get(j);
+            }
+        }
+        Planet emptyPlanet = new Planet();
+        return emptyPlanet;
+    }
+
+    // Get the index of the planet
+    public int getIndexOfPlanet(Planet P){
+        return this.node.indexOf(P);
     }
 }
